b798cda6992603aff57759ab850f1eac011a4852
santosh2711 <absmods538@gmail.com>
2025-11-23 17:42:16 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,15 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBQzrBURFwenjWEEnQMMIH7sOSqPq0Qtx0",
-      authDomain: "unm-caf-orders.firebaseapp.com",
-      projectId: "unm-caf-orders",
-      storageBucket: "unm-caf-orders.appspot.com",
-      messagingSenderId: "562096834985",
-      appId: "1:562096834985:web:f4bfe451e5482521e49cb8",
-      measurementId: "G-J12PBDK7N1"
-    };
-
+  apiKey: "AIzaSyBQZrBURFwenjWEEnQMMIH7sOSqPq0Qtx0",
+  authDomain: "unm-caf-orders.firebaseapp.com",
+  projectId: "unm-caf-orders",
+  storageBucket: "unm-caf-orders.appspot.com",
+  messagingSenderId: "562096834985",
+  appId: "1:562096834985:web:f4fbe451e5482521e49cb8",
+  measurementId: "G-J12PBDK7N1"
+});
 
 const messaging = firebase.messaging();
 
