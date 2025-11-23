19324723f94bcf6495689f2aa09bea1552ef4acc
santosh2711 <absmods538@gmail.com>
2025-11-23 13:58:23 +0800

    Add Firebase messaging service worker setup

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBQZrBURFwenjWEEnQMMIH7sOSqPq0Qtx0",
+  authDomain: "unm-caf-orders.firebaseapp.com",
+  projectId: "unm-caf-orders",
+  storageBucket: "unm-caf-orders.appspot.com", // fixed typo here
+  messagingSenderId: "562096834985",
+  appId: "1:562096834985:web:f4bfe451e5482521e49cb8",
+  measurementId: "G-J12PBDK7N1"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/icon.png' // Optional: your notification icon
+  });
+});
