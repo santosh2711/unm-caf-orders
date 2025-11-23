d8be9f1bd1726bc5e76d17ea0573c789bfa19f7d
santosh2711 <absmods538@gmail.com>
2025-11-23 17:25:47 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,9 +7,10 @@ firebase.initializeApp({
   projectId: "unm-caf-orders",
   storageBucket: "unm-caf-orders.appspot.com",
   messagingSenderId: "562096834985",
-  appId: "1:562096834985:web:f4fbe451e5482521e49cb8",
+  appId: "1:562096834985:web:f4bfe451e5482521e49cb8",
   measurementId: "G-J12PBDK7N1"
 });
+
 
 const messaging = firebase.messaging();
 
