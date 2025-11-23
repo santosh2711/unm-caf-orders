4503256ef94f5403767b0e13cd82af6598ef40f1
santosh2711 <absmods538@gmail.com>
2025-11-23 19:16:09 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,23 +2,23 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBQzrBURFwenjWEEnQMMIH7sOSqPq0Qtx0",
-      authDomain: "unm-caf-orders.firebaseapp.com",
-      projectId: "unm-caf-orders",
-      storageBucket: "unm-caf-orders.appspot.com",
-      messagingSenderId: "562096834985",
-      appId: "1:562096834985:web:f4bfe451e5482521e49cb8",
-      measurementId: "G-J12PBDK7N1"
-    });
+  apiKey: "AIzaSyBQzrBURFwenjWEnQMMIH7sOSqPq0Q0tx0",
+  authDomain: "unm-caf-orders.firebaseapp.com",
+  projectId: "unm-caf-orders",
+  storageBucket: "unm-caf-orders.appspot.com",
+  messagingSenderId: "562096834985",
+  appId: "1:562096834985:web:f4bfe451e5482521e49cb8",
+  measurementId: "G-J12PBDK7N1"
+});
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification(
-    payload.notification.title,
-    {
-      body: payload.notification.body,
-      icon: '/favicon.ico'
-    }
-  );
+  const title = payload.notification?.title || payload.data.title;
+  const body = payload.notification?.body || payload.data.body;
+
+  self.registration.showNotification(title, {
+    body: body,
+    icon: 'favicon.ico'
+  });
 });
