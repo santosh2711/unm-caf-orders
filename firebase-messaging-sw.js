cb24bdc7171b91904fb84e4a8a18076914825a86
santosh2711 <absmods538@gmail.com>
2025-11-23 17:21:50 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,17 +5,20 @@ firebase.initializeApp({
   apiKey: "AIzaSyBQZrBURFwenjWEEnQMMIH7sOSqPq0Qtx0",
   authDomain: "unm-caf-orders.firebaseapp.com",
   projectId: "unm-caf-orders",
-  storageBucket: "unm-caf-orders.appspot.com", // fixed typo here
+  storageBucket: "unm-caf-orders.appspot.com",
   messagingSenderId: "562096834985",
-  appId: "1:562096834985:web:f4bfe451e5482521e49cb8",
+  appId: "1:562096834985:web:f4fbe451e5482521e49cb8",
   measurementId: "G-J12PBDK7N1"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: '/icon.png' // Optional: your notification icon
-  });
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: '/favicon.ico'
+    }
+  );
 });
